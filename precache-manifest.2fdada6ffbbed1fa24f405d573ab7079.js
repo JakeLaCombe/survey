617f7d6ab86645c5b371080bf1795e350ca0bc15
Jake LaCombe <jake.lacombe2@gmail.com>
2020-02-11 00:08:57 -0600

    Initial dist subtree commit

---
+++ precache-manifest.2fdada6ffbbed1fa24f405d573ab7079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64f7b2718abd055f3915ee819988e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3258b0cb575ef905980",
+    "url": "/static/css/main.db9d7179.chunk.css"
+  },
+  {
+    "revision": "0530130493df5ec8005a",
+    "url": "/static/js/2.01e9f82f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.01e9f82f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3258b0cb575ef905980",
+    "url": "/static/js/main.e5ad991f.chunk.js"
+  },
+  {
+    "revision": "464c8a6eee638933db9f",
+    "url": "/static/js/runtime-main.c83a4d79.js"
+  }
+]);
