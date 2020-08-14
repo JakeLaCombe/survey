f47e22d4106651052fc17ff692ed80da13beed08
Jake LaCombe <jake.lacombe2@gmail.com>
2020-08-14 17:18:53 -0500

    Fix Feedback Issues

--- src/components/Survey/index.jsx
+++ src/components/Survey/index.jsx
@@ -1,63 +1,108 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Survey.css';
-import QuestionNumber from '../QuestionNumber';
-import SurveyQuestion from '../SurveyQuestion';
-import { CSSTransition } from 'react-transition-group';
-import questions from '../questions'
+import React, { useState, useEffect, useRef } from "react";
+import "./Survey.css";
+import QuestionNumber from "../QuestionNumber";
+import SurveyQuestion from "../SurveyQuestion";
+import { CSSTransition } from "react-transition-group";
+import questions from "../questions";
 
 function Survey({ onSurveyFinished }) {
-    let [answers, setAnswers] = useState({});
-    let [currentQuestion, setCurrentQuestion] = useState(0)
-    let [showQuestion, setShowQuestion] = useState(false)
-    let [showActions, setShowActions] = useState(true)
-    let showFirstQuestion = useRef(true);
+  let [answers, setAnswers] = useState({});
+  let [currentQuestion, setCurrentQuestion] = useState(0);
+  let [showQuestion, setShowQuestion] = useState(false);
+  let [showActions, setShowActions] = useState(true);
+  let showFirstQuestion = useRef(true);
 
-    useEffect(() => {
-        if (showFirstQuestion.current) {
-            setTimeout(setShowQuestion(true), 0);
-            showFirstQuestion.current = false;
-        }
-    }, [showFirstQuestion])
+  useEffect(() => {
+    if (showFirstQuestion.current) {
+      setTimeout(setShowQuestion(true), 0);
+      showFirstQuestion.current = false;
+    }
+  }, [showFirstQuestion]);
 
-    return (
-        <>
-            <QuestionNumber questionNumber={currentQuestion + 1} />
-            <div style={{ position: 'relative' }}>
-                <CSSTransition
-                    in={showQuestion}
-                    timeout={300}
-                    classNames="alert"
-                    onEntered={() => { setShowActions(true) }}
-                >
-                    {showQuestion ? (<SurveyQuestion
-                        key={questions[currentQuestion].id}
-                        value={answers[currentQuestion]}
-                        question={questions[currentQuestion]} onChange={(value) => {
-                            setAnswers({ ...answers, [currentQuestion]: value })
-                        }} />) : (<div></div>)}
-                </CSSTransition>
-            </div>
-            {showActions &&
-                <div className="question-actions">
-                    {currentQuestion !== 0 && <button className="prev-button" key="prev" onClick={() => {
-                        setCurrentQuestion(currentQuestion -= 1)
-                        setShowActions(false)
-                        setShowQuestion(false)
-                        setTimeout(() => { setShowQuestion(true) }, 100)
-                    }}> <span className="btn__content" tabIndex="-1">{'<<<'}&nbsp;&nbsp;&nbsp;&nbsp;Previous</span></button>}
-                    {currentQuestion !== questions.length - 1 && <button className="next-button" key="next" disabled={answers[currentQuestion.toString()] === undefined} onClick={() => {
-                        setCurrentQuestion(currentQuestion += 1)
-                        setShowActions(false)
-                        setShowQuestion(false)
-                        setTimeout(() => { setShowQuestion(true) }, 100)
-                    }}><span className="btn__content" tabIndex="-1">Next&nbsp;&nbsp;&nbsp;&nbsp;{'>>>'}</span></button>}
-                    {currentQuestion === questions.length - 1 && <button className="next-button" key="next" disabled={answers[currentQuestion.toString()] === undefined} onClick={() => {
-                        onSurveyFinished(answers);
-                    }}><span className="btn__content" tabIndex="-1">Finish</span></button>}
-                </div>
-            }
-        </>
-    );
+  return (
+    <>
+      <QuestionNumber questionNumber={currentQuestion + 1} />
+      <div style={{ position: "relative" }}>
+        <CSSTransition
+          in={showQuestion}
+          timeout={300}
+          classNames="alert"
+          onEntered={() => {
+            setShowActions(true);
+          }}
+        >
+          {showQuestion ? (
+            <SurveyQuestion
+              key={questions[currentQuestion].id}
+              value={answers[currentQuestion]}
+              question={questions[currentQuestion]}
+              onChange={(value) => {
+                setAnswers({ ...answers, [currentQuestion]: value });
+              }}
+            />
+          ) : (
+            <div></div>
+          )}
+        </CSSTransition>
+      </div>
+      {showActions && (
+        <div className="question-actions">
+          {currentQuestion !== 0 && (
+            <button
+              className="prev-button"
+              key="prev"
+              onClick={() => {
+                setCurrentQuestion((currentQuestion -= 1));
+                setShowActions(false);
+                setShowQuestion(false);
+                setTimeout(() => {
+                  setShowQuestion(true);
+                }, 100);
+              }}
+            >
+              {" "}
+              <span className="btn__content" tabIndex="-1">
+                {"<<<"}&nbsp;&nbsp;&nbsp;&nbsp;Previous
+              </span>
+            </button>
+          )}
+          {currentQuestion !== questions.length - 1 && (
+            <button
+              className="next-button"
+              key="next"
+              disabled={answers[currentQuestion.toString()] === undefined}
+              onClick={() => {
+                setCurrentQuestion((currentQuestion += 1));
+                setShowActions(false);
+                setShowQuestion(false);
+                setTimeout(() => {
+                  setShowQuestion(true);
+                }, 100);
+              }}
+            >
+              <span className="btn__content" tabIndex="-1">
+                Next&nbsp;&nbsp;&nbsp;&nbsp;{">>>"}
+              </span>
+            </button>
+          )}
+          {currentQuestion === questions.length - 1 && (
+            <button
+              className="next-button"
+              key="next"
+              disabled={answers[currentQuestion.toString()] === undefined}
+              onClick={() => {
+                onSurveyFinished(answers);
+              }}
+            >
+              <span className="btn__content" tabIndex="-1">
+                Finish
+              </span>
+            </button>
+          )}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Survey;
--- src/components/questions.js
+++ src/components/questions.js
@@ -1,112 +1,103 @@
 export default [
-    {
-        id: 0,
-        title: 'What is your current employment status?',
-        answers: [
-            'I am a current full time federal employee',
-            'I am currently on active duty in the military',
-            'I am a part time federal employee or national guard/reserve military member',
-            'I am a retired military or federal employee'
-        ]
-    },
-    {
-        id: 1,
-        title: 'How old are you',
-        answers: [
-            '17-30',
-            '30-45',
-            '45-55',
-            '56+'
-        ]
-    },
-    {
-        id: 2,
-        title: 'How many years until you expect to retire?',
-        answers: [
-            '20 years or more',
-            '10 - 20 years',
-            '5 - 10 years',
-            'Less than 5 years or currently retired'
-        ]
-    },
-    {
-        id: 3,
-        title: 'How much do you currently have in your savings?',
-        answers: [
-            '6 months or more of living expenses',
-            '3-5 months living expenses',
-            '1-2 months living expenses',
-            'Less than one month of living expenses'
-        ]
-    },
-    {
-        id: 4,
-        title: 'If the stock marker lost 40% you would',
-        answers: [
-            'See it as an opportunty and invest more if you had the cash available',
-            'Continue with my currenty strategy, but not invest any more even if I had cash available',
-            'Change my strategy and reduce my exposure to risk',
-            'Pull my money out of the stock market',
-        ]
-    },
-    {
-        id: 5,
-        title: 'In the next few years you expect your income to',
-        answers: [
-            'Increase',
-            'Stay the same',
-            'Decrease',
-            'Decrease drastically'
-        ]
-    },
-    {
-        id: 6,
-        title: 'What do you expect out of your TSP? The word volatility is the rate at which your tsp account increases or decreases over a given period of time.',
-        answers: [
-            'I expect my portfolio to significantly exceed inflation, even if this means higher levels of volatility',
-            'I expect my portfolio to exceed inflation, with reasonable levels of volatility',
-            'I exepct my portfolio to keep pace with inflation, with lower levels of volatility',
-            'I am more concerned with having as little volatility as possible more than keeping pace with inflation'
-        ]
-    },
-    {
-        id: 7,
-        title: "I'm most comfortable with a portfolio that",
-        answers: [
-            'May fluctuate by 20% up or down but averages 10% over the life of the investment',
-            'May fluctuate by 15% up or down but averages 8% over the life of the investment',
-            'May fluctuate by 10% up or down but averages 4-6% over the life of the investment',
-            'I would take less than a 2% total return for my portfolio to fluctuate a minimal amount.',
-        ]
-    },
-    {
-        id: 8,
-        title: 'How would you describe yourself?',
-        answers: [
-            'I am an optimist. I am confident that things will always work out.',
-            "I am objective. Show me the benefits and the drawbacks. I'll make a decision and live with it",
-            'I am an anxious person. I usually worry about most things',
-            'I am a pessimist. If something can go wrong, it will go wrong.'
-        ]
-    },
-    {
-        id: 9,
-        title: 'When you are reviewing your TSP what will you notice first?',
-        answers: [
-            'The overall longterm performance of the entire portfolio',
-            'The most recent results of the entire portfolio',
-            'The individual fund that is doing the best',
-            'The individual fund that is doing the worst',
-        ]
-    },
-    {
-        id: 10,
-        title: 'If I was given $10,000 in an inheritance I would',
-        answers: [
-            'Wait for a drop in the market and invest the money according to my current investment strategy',
-            'Invest the money immediately in accordance with my current investment strategy',
-            'Invest the money in Low Risk Bonds or Certificates of Deposit',
-            'Save the money and not invest any of it.'
-        ]
-    },
-]
+  {
+    id: 0,
+    title: "What is your current employment status?",
+    answers: [
+      "I am a current full time federal employee",
+      "I am currently on active duty in the military",
+      "I am a part time federal employee or national guard/reserve military member",
+      "I am a retired military or federal employee",
+    ],
+  },
+  {
+    id: 1,
+    title: "How old are you",
+    answers: ["17-30", "30-45", "45-55", "56+"],
+  },
+  {
+    id: 2,
+    title: "How many years until you expect to retire?",
+    answers: [
+      "20 years or more",
+      "10 - 20 years",
+      "5 - 10 years",
+      "Less than 5 years or currently retired",
+    ],
+  },
+  {
+    id: 3,
+    title: "How much do you currently have in your savings?",
+    answers: [
+      "6 months or more of living expenses",
+      "3-5 months living expenses",
+      "1-2 months living expenses",
+      "Less than one month of living expenses",
+    ],
+  },
+  {
+    id: 4,
+    title: "If the stock market lost 40% you would",
+    answers: [
+      "See it as an opportunty and invest more if you had the cash available",
+      "Continue with my currenty strategy, but not invest any more even if I had cash available",
+      "Change my strategy and reduce my exposure to risk",
+      "Pull my money out of the stock market",
+    ],
+  },
+  {
+    id: 5,
+    title: "In the next few years you expect your income to",
+    answers: ["Increase", "Stay the same", "Decrease", "Decrease drastically"],
+  },
+  {
+    id: 6,
+    title:
+      "What do you expect out of your TSP? The word volatility is the rate at which your tsp account increases or decreases over a given period of time.",
+    answers: [
+      "I expect my portfolio to significantly exceed inflation, even if this means higher levels of volatility",
+      "I expect my portfolio to exceed inflation, with reasonable levels of volatility",
+      "I exepct my portfolio to keep pace with inflation, with lower levels of volatility",
+      "I am more concerned with having as little volatility as possible more than keeping pace with inflation",
+    ],
+  },
+  {
+    id: 7,
+    title: "I'm most comfortable with a portfolio that",
+    answers: [
+      "May fluctuate by 20% up or down but averages 10% over the life of the investment",
+      "May fluctuate by 15% up or down but averages 8% over the life of the investment",
+      "May fluctuate by 10% up or down but averages 4-6% over the life of the investment",
+      "I would take less than a 2% total return for my portfolio to fluctuate a minimal amount.",
+    ],
+  },
+  {
+    id: 8,
+    title: "How would you describe yourself?",
+    answers: [
+      "I am an optimist. I am confident that things will always work out.",
+      "I am objective. Show me the benefits and the drawbacks. I'll make a decision and live with it",
+      "I am an anxious person. I usually worry about most things",
+      "I am a pessimist. If something can go wrong, it will go wrong.",
+    ],
+  },
+  {
+    id: 9,
+    title: "When you are reviewing your TSP what will you notice first?",
+    answers: [
+      "The overall longterm performance of the entire portfolio",
+      "The most recent results of the entire portfolio",
+      "The individual fund that is doing the best",
+      "The individual fund that is doing the worst",
+    ],
+  },
+  {
+    id: 10,
+    title: "If I was given $10,000 in an inheritance I would",
+    answers: [
+      "Wait for a drop in the market and invest the money according to my current investment strategy",
+      "Invest the money immediately in accordance with my current investment strategy",
+      "Invest the money in Low Risk Bonds or Certificates of Deposit",
+      "Save the money and not invest any of it.",
+    ],
+  },
+];
